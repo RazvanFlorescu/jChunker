7bb7f61a6a5a3d2e169797bc4913fe81463c6012
Florescu Razvan <rikimoo@users.noreply.github.com>
2018-01-12 01:04:46 +0200

    Update NerTimeEntity

--- jchunker-app/src/main/java/core/ner/NerTimeEntity.java
+++ jchunker-app/src/main/java/core/ner/NerTimeEntity.java
@@ -1,12 +1,14 @@
 package core.ner;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-import core.Shared.MergedEntity;
 import core.token.Token;
 
-public class NerTimeEntity  implements Recognizer
+public class NerTimeEntity implements Recognizer
 {
 	private File gazeteerFile;
 	
@@ -20,7 +22,27 @@ public class NerTimeEntity  implements Recognizer
 	@Override
 	public NameEntity recognize(Token token)
 	{
-		return null;
+		 NameEntity resultExpectation = new NameEntity();
+		   resultExpectation.setId(token.getId());
+		   resultExpectation.setType("time");
+		   resultExpectation.setWord(token.getWord());
+		   
+		   try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(gazeteerFile.getAbsolutePath()), "UTF-8"));){
+			   String line = br.readLine();
+			   while(line!=null){
+				   if(line.contains(token.getWord().toLowerCase()) && line.toLowerCase().replace(" ","").length()== token.getWord().toLowerCase().replace(" ","").length()){
+					   return resultExpectation;
+				   }
+				   line=br.readLine();
+			   }
+			   
+		   } catch (IOException x) {
+			    System.err.format("IOException: %s%n", x);
+		   }
+	   		
+			
+			return null;
 	}
-
 }
+
+
