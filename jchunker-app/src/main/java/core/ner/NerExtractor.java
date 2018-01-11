18e687784a6e34db520e01fcb43ee23983f849a9
Florescu Razvan <rikimoo@users.noreply.github.com>
2018-01-12 01:11:32 +0200

    Added NerExtractor

--- jchunker-app/src/main/java/core/ner/NerExtractor.java
+++ jchunker-app/src/main/java/core/ner/NerExtractor.java
@@ -1,8 +1,9 @@
 package core.ner;
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import core.token.Token;
 import core.token.TokenizedText;
 
 public class NerExtractor
@@ -13,8 +14,10 @@ public class NerExtractor
 	
 	public NerExtractor() throws IOException 
 	{
-		nerTimeEntity = new NerTimeEntity("C:/path1/path2/gazeteiera.txt");
-		// same for the other recognizers
+		nerTimeEntity = new NerTimeEntity("src\\main\\resources\\gazeteer\\time.txt");
+		nerOrganizationEntity = new NerOrganizationEntity("src\\main\\resources\\gazeteer\\organization.txt"); ;
+		nerPeopleName = new NerPeopleNameEntity("src\\main\\resources\\gazeteer\\personNames");;
+		
 	}
 	
 	/**
@@ -25,8 +28,28 @@ public class NerExtractor
 	 */
 	public List<NerText> extract(TokenizedText tokenizedText)
 	{
-		// use here the nerTimeEntity 
-		return null;
+	  List<NerText>result = new ArrayList<>();	
+	  
+	  NerText peopleName = new NerText();
+	  NerText organization = new NerText();
+	  NerText time = new NerText();
+	  
+		  for (Token token : tokenizedText.getTokens()) {
+			if(this.nerOrganizationEntity.recognize(token)!=null){
+				organization.getNameEntities().add(this.nerOrganizationEntity.recognize(token));
+			}
+			if(this.nerPeopleName.recognize(token)!=null){
+				peopleName.getNameEntities().add(this.nerPeopleName.recognize(token));
+			}
+			if(this.nerTimeEntity.recognize(token)!=null){
+			    time.getNameEntities().add(this.nerTimeEntity.recognize(token));
+			}
+		}
+		result.add(peopleName);
+		result.add(organization);
+		result.add(time);
+        
+		return result;
 	}
 
 }
